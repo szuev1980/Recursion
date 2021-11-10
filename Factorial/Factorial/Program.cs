028bec5d973a8b518d40122eddc7944ac37b9c4b
szuev1980 <87873223+szuev1980@users.noreply.github.com>
2021-11-10 19:05:59 +0200

    add recurtion simple

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Rec(int i)
+        {
+            if (i > 1) Rec(--i);
+            Console.WriteLine("***");
+        }
+
+        static void Main(string[] args) 
+        {
+            Rec(3);
+        }
+    }
+}
